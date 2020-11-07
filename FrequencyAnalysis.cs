cdd72400508cf23d789c6b52c6ba65ae0a868d41
Ofsouzap <ofsouzap@gmail.com>
2020-11-07 13:01:13 +0000

    Initial commit

---
+++ FrequencyAnalysis.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffineDecipher
+{
+    class FrequencyAnalysis
+    {
+
+        public static Dictionary<char, int> GetTextCharFrequency(string text,
+            char[] countedChars)
+        {
+
+            Dictionary<char, int> charFrequency = new Dictionary<char, int>();
+
+            foreach (char c in text)
+            {
+
+                if (!countedChars.Contains(c)) continue;
+
+                if (charFrequency.ContainsKey(c))
+                {
+
+                    charFrequency[c]++;
+
+                }
+                else
+                {
+
+                    charFrequency.Add(c, 1);
+
+                }
+
+            }
+
+            return charFrequency;
+
+        }
+
+        public static Dictionary<char, double> CharFrequencyToCharProportion(Dictionary<char, int> charFrequency)
+        {
+
+            int totalChars = 0;
+
+            foreach (char c in charFrequency.Keys) totalChars += charFrequency[c];
+
+            Dictionary<char, double> charProportion = new Dictionary<char, double>();
+
+            foreach (char c in charFrequency.Keys)
+            {
+
+                charProportion[c] = (double)charFrequency[c] / totalChars;
+
+            }
+
+            return charProportion;
+
+        }
+
+        public static double GetKeyFrequencyDifference<T>(Dictionary<T, double> a, Dictionary<T, double> b)
+        {
+
+            double difference = 0;
+
+            foreach (T key in a.Keys)
+            {
+
+                double aValue = a.ContainsKey(key) ? a[key] : 0;
+                double bValue = b.ContainsKey(key) ? b[key] : 0;
+
+                difference += Math.Abs(a[key] - b[key]);
+
+            }
+
+            return difference;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+/*Heavily based off of VignereDeciphering project*/
+
+namespace AffineDecipher
+{
+    class Program
+    {
+
+        public static readonly int[] multiplierOptions = new int[]
+        {
+            1,
+            3,
+            5,
+            7,
+            9,
+            11,
+            15,
+            17,
+            19,
+            21,
+            23,
+            25
+        };
+        public static readonly char[] validCharacters = new char[]
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z'
+        };
+
+        static void Main(string[] args)
+        {
+
+            string text;
+
+            Console.WriteLine("Text:");
+            string input = Console.ReadLine();
+
+            if (input[0] == '\\')
+            {
+                text = GetTextFromFile(input.Substring(1)).ToLower();
+            }
+            else
+            {
+                text = input.ToLower();
+            }
+
+            List<Decoding> decodings = GetAllDecodings(text);
+
+            decodings.Sort((valueA, valueB) => valueA.difference.CompareTo(valueB.difference));
+            
+            for (int i = 0; i < decodings.Count; i++)
+            {
+
+                Decoding decoding = decodings[i];
+
+                Console.WriteLine($"Difference: {decoding.difference}");
+                Console.WriteLine($"Decrypted by: {decoding.multiplier}x + {decoding.offset}");
+
+                int inverseMultiplier = InverseMultiplier(decoding.multiplier);
+                int decryptOffset = (inverseMultiplier * (-decoding.offset));
+                decryptOffset = (decryptOffset + ((Math.Abs(decryptOffset) / validCharacters.Length) * validCharacters.Length) + validCharacters.Length) % validCharacters.Length;
+
+                Console.WriteLine($"Encrypted by: {inverseMultiplier}x + {decryptOffset}");
+                Console.WriteLine(decoding.text);
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Program Finished");
+            Console.ReadKey();
+
+        }
+
+        static string GetTextFromFile(string filename)
+        {
+            Debug.Assert(File.Exists(filename));
+            return File.ReadAllText(filename);
+        }
+
+        static List<Decoding> GetAllDecodings(string text)
+        {
+
+            List<Decoding> decodingsList = new List<Decoding>();
+
+            foreach (int multiplier in multiplierOptions)
+            {
+
+                for (int offset = 0; offset < 26; offset++)
+                {
+
+                    string decodeText = EncryptText(text,
+                        multiplier,
+                        offset);
+
+                    double decodeDifference = FrequencyAnalysis.GetKeyFrequencyDifference(
+                        FrequencyAnalysis.CharFrequencyToCharProportion(FrequencyAnalysis.GetTextCharFrequency(decodeText, validCharacters)),
+                        EnglishLetterFrequency.GetLetterProportions()
+                    );
+
+                    decodingsList.Add(new Decoding(
+                        decodeText,
+                        multiplier,
+                        offset,
+                        decodeDifference
+                    ));
+
+                }
+
+            }
+
+            return decodingsList;
+
+        }
+
+        static string EncryptText(string text,
+            int multiplier,
+            int offset)
+        {
+
+            string output = "";
+
+            foreach (char c in text)
+            {
+                output = output + EncryptChar(c, multiplier, offset);
+            }
+
+            return output;
+
+        }
+
+        static char EncryptChar(char c,
+            int multiplier,
+            int offset)
+        {
+
+            if (!validCharacters.Contains(c)) { return c; }
+
+            int pos = Array.IndexOf(validCharacters, c);
+            pos = (((multiplier * pos) + offset) + validCharacters.Length) % validCharacters.Length;
+            return validCharacters[pos];
+
+        }
+
+        public static int InverseMultiplier(int origMultiplier)
+        {
+
+            int baseNumber = validCharacters.Length;
+
+            for (int i = 0; i < baseNumber; i++)
+            {
+
+                if ((i * origMultiplier) % baseNumber == 1) return i;
+
+            }
+
+            return 0;
+
+        }
+
+        private struct Decoding
+        {
+
+            public string text;
+            public int multiplier;
+            public int offset;
+            public double difference;
+
+            public Decoding(string text, int multiplier, int offset, double difference)
+            {
+                this.text = text;
+                this.multiplier = multiplier;
+                this.offset = offset;
+                this.difference = difference;
+            }
+
+        }
+
+    }
+}
